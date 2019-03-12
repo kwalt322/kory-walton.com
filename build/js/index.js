c912042598c1f433e49d1211ab6a610d007c5511
Kory Walton <kwalton@archer-group.com>
2019-03-11 20:50:05 -0400

    hoagiefest and honest pets calendar content

--- build/js/index.js
+++ build/js/index.js
@@ -4,3 +4,14 @@
     $('.animated-icon').toggleClass('open');
   });
 
+// pathetic password validation
+
+$('#validate').on('click', function() {
+if (password = "rawr")
+{alert('Correct!')}
+else
+{alert('Wrong Password')}
+})
+
+
+
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -73,6 +73,14 @@
 // mobile nav
 $('.second-button').on('click', function () {
   $('.animated-icon').toggleClass('open');
+}); // pathetic password validation
+
+$('#validate').on('click', function () {
+  if (password = "rawr") {
+    alert('Correct!');
+  } else {
+    alert('Wrong Password');
+  }
 });
 
 /***/ })
