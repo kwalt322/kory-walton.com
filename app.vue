8826ab57f0e8cf847151ec5d331d6efbfda9aac9
Kory Walton <kory.walton@bounteous.com>
2020-10-23 14:55:31 -0400

    Starting Parcel/Yarn/Vue Build

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p class="red">just text</p>
+    <p>another</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      bundler: "Parcel",
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.red {
+  color: green;
+}
+</style>
